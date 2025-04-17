773bb47fb199944cdced805497eb3d36bc142e99
Luis Montoya <luislmmc2408@gmail.com>
2025-04-17 16:54:28 -0500

    04-multiplitacion

---
+++ Documents/proyectos fernando/cursos/node-fernando-h/04-multiplication/dist/config/plugins/args.plugin.js
@@ -0,0 +1,10 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.yarg = void 0;
+const yargs_1 = __importDefault(require("yargs"));
+const helpers_1 = require("yargs/helpers");
+exports.yarg = (0, yargs_1.default)((0, helpers_1.hideBin)(process.argv))
+    .parseSync();
---
+++ Documents/proyectos fernando/cursos/node-fernando-h/04-multiplication/src/app.ts
@@ -0,0 +1,18 @@
+import { yarg  } from "./config/plugins/args.plugin"
+
+// console.log(process.argv)
+// console.log(yarg.b)
+
+
+(async() => {
+
+   await main()
+   console.log('finalizando app')
+
+
+})()
+
+async function main() {
+
+    
+}
